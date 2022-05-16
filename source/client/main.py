4caae69d4738f52fb3ca1342f9e8a14e338f0e0b
dotrann1412 <dotrann.1412@gmail.com>
2022-05-16 21:32:05 +0700

    update client main

--- source/client/main.py
+++ source/client/main.py
@@ -9,83 +9,42 @@ import datetime
 import socket
 
 from common import global_definition
-from common.request import Request
-from common.data import Data
 import json
-sample_request = []
 
-sample_request.append(Request.create_request(
-    Request.Type.UPDATE,
-    Data.Type.SINGLE_ID, 
-    'C000003'
-))
-
-
-for req in sample_request:
-    print(req.to_string())
-
-import ast
-import cv2
-import numpy as np
-
-
-def is_still_connected(sock: socket.socket):
-    try:
-        sock.sendall(b"ping")
-        return True
-    except:
-        return False
-
-def listen_for_new_message(socket: socket.socket):
-    while True:
-        header_bstr = socket.recv(4)
-
-        if header_bstr == b'ping':
-            continue
-
-        message_len = int.from_bytes(header_bstr, 'little')
-        message_len = (message_len + 1023) // 1024
-        message_str = ''
-
-        for i in range (message_len):
-            message = socket.recv(1024).decode()
-            message_str = message_str + message
-
-        jsvalue = json.dumps(message_str)
-        print(jsvalue)
-
-def communicate(socket: socket.socket):
-    while True:
-        message = input()
-        if message.lower() == 'q':
-            break
-        socket.send(message.encode())
-
-def send_request(socket: socket.socket, requets_message, function):
-    '''
-        Return: status (failed or successfuly)
-        Flow: send -> 
-    '''
-
-    pass
-
-def receive_response(socket: socket.socket):
-    '''
-        Return: map response data with request id (failed or successfuly)
-    ''' 
+def request_to_server(appsocket: socket.socket, request: dict):
+    request = str(request).encode()
     
-    pass
+    try:
+        socket.send(request)
+    except socket.timeout:
+        pass
 
-def run_client():
-    client = connection.Connection(ip_address = global_definition.HOST, port = global_definition.PORT)
-    socket = client.get_socket()
+    BYTES_PER_BLOCK = 1024
+    
+    response_data = None
 
-    recv = threading.Thread(target = listen_for_new_message, args = (socket, ))
-    communicate_thread = threading.Thread(target = communicate, args = (socket, ))
+    while True:
+        try:
+            response_header = appsocket.recv(4)
+            
+            if response_header == b'ping':
+                continue
+            
+            response_len = int.from_bytes(response_header, 'little')
+            message_block_count = (response_len + BYTES_PER_BLOCK - 1) // BYTES_PER_BLOCK
 
-    recv.start()
-    communicate_thread.start()
+            response_data_str = ''
+
+            for i in range(message_block_count):
+                response_data = response_data + appsocket.recv(BYTES_PER_BLOCK).decode('utf-8')
+
+            response_data = json.loads(response_data_str)
+
+            break
+        except socket.timeout:
+            pass # do something
+
+    return response_data
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=run_client, args = ())
-    thread.start()
+    print('hehe')
