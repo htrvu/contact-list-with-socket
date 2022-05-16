f19fcbe6188895e06d8ad7027019b9e2706aae09
dotrann1412 <dotrann.1412@gmail.com>
2022-05-16 21:33:35 +0700

    update factory

--- source/server/factory.py
+++ source/server/factory.py
@@ -4,11 +4,10 @@ import json
 
 sys.path.append('..')
 
-from common.contact import Contact
-from common.request import Request
-from common.response import Response
-from common.data import Data
-
+from common.request import RequestType
+from common.utils import base64_encode
+from common.utils import print_color, text_format
+from connection import is_still_connected
 
 CONTACT_FILE_PATH = '../data/contact_data.json'
 LARGE_IMAGE_DIR = '../data/large_image'
@@ -19,57 +18,93 @@ contact_dict = {}
 
 with open(CONTACT_FILE_PATH, 'r') as fp:
     jsvalue = json.load(fp)
+
     for key in jsvalue:
-        contact_list.append(Contact(key, jsvalue[key]))
-        contact_dict[key] = Contact(key, jsvalue[key])
+        small_image_b64 = None
+        large_image_b64 = None
 
-def parse_small_image(contact: Contact):
-    ret_contact = contact.copy()
+        small_image_path = os.path.join(SMALL_IMAGE_DIR, jsvalue[key]['image'])
+        large_image_Path = os.path.join(LARGE_IMAGE_DIR, jsvalue[key]['image'])
+
+        if os.path.exists(small_image_path):
+            with open(small_image_path, 'rb') as fp:
+                small_image_b64 = base64_encode(fp.read())
+
+        if os.path.exists(large_image_Path, 'rb'):
+            with open(small_image_path) as fp:
+                small_image_b64 = base64_encode(fp.read())
+
+        mini_contact = {
+            'id': key,
+            'name': jsvalue[key]['name'],
+            'image': small_image_b64
+        }
+
+        full_contact = {
+            'id': key,
+            'name': jsvalue[key]['name'],
+            'image': large_image_b64,
+            'phone_numer': jsvalue[key]['phone_numer'],
+            'email': jsvalue[key]['email'],
+            'bio': jsvalue[key]['bio']
+        }
+        contact_list.append(mini_contact)
+        contact_dict[key] = full_contact
+
+def create_response(request: dict):
+    response_data = None
+
+    if request['dtype'] == RequestType.BLOCK:
+        block_id = request['block_id']
+        
+        if block_id == -1:
+            response_data = {
+                'status': 'ok',
+                'type' : 'block',
+                'block_id': block_id,
+                'data': contact_list
+            }
+        elif block_id >= 0:
+            blocksize = RequestType.BLOCK_SIZE
+            left = block_id * blocksize
+            right = min(left + blocksize, len(contact_list))
+
+            if left < right:
+                response_data = {
+                    'status': 'ok',
+                    'type' : 'block',
+                    'block_id': block_id,
+                    'data': contact_list[left:right]
+                }
+
+    elif request['dtype'] == RequestType.SINGLE_ID:
+        id = request['id']
+        if id in contact_dict:
+            response_data = {
+                'status': 'ok',
+                'type': RequestType.SINGLE_ID,
+                'data': contact_dict[id]
+            }
+        else:
+            response_data = {
+                'status': f'{id} not found in contact list'
+            }
+    else:
+        raise Exception('Invalid request type')
     
-    print(contact.get_name())
+    return response_data
 
-    image = {
-        'small_image': open(os.path.join(SMALL_IMAGE_DIR, contact.get_image()), 'rb').read()
-    }
-
-    ret_contact.set_image(image)
-
-    return ret_contact
-
-def parse_large_image(contact: Contact):
-    ret_contact = contact
+def reply_request(appsocket: socket.socket, request: dict):
+    message_to_send = str(create_response(request)).encode()
+    message_len = len(message_to_send).to_bytes(4, 'little')
     
-    image = {
-        'large_image': open(os.path.join(LARGE_IMAGE_DIR, contact.get_image()), 'rb').read()
-    }
-
-    ret_contact.set_image(image)
-
-    return ret_contact
-
-def parse_image(contact: Contact):
-    ret_contact = contact
-
-    image = {
-        'small_image': open(os.path.join(SMALL_IMAGE_DIR, contact.get_image()), 'rb').read(),
-        'large_image': open(os.path.join(LARGE_IMAGE_DIR, contact.get_image()), 'rb').read()
-    }
-
-    ret_contact.set_image(image)
-
-    return ret_contact
-
-def reply_request(socket: socket.socket, request: Request):
-    contact = None
-    if (request.get_data_type() == Data.Type.SINGLE_ID):
-        contact = contact_dict[request.get_index()]
-
-    _contact = contact.copy()
-    print(_contact.to_string())
-    
-    message_len = len(_contact.to_string().encode())
-    message_len = message_len.to_bytes(4, 'little')
-    
-    socket.send(message_len)
-    socket.send(_contact.to_string().encode())
-    print('Message length: ', len(_contact.to_string().encode()))
+    try:
+        if not is_still_connected(appsocket):
+            appsocket.close()
+            return
+        
+        appsocket.send(message_len)
+        appsocket.send(message_to_send)
+        print_color('Packet sent', text_format.OKGREEN)
+    except socket.error as err:
+        print_color(err, text_format.FAIL)
