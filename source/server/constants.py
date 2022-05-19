4acd813075c18b94fd4816767ec8eddf8d53d575
dotrann1412 <dotrann.1412@gmail.com>
2022-05-20 00:15:55 +0700

    update datapath

--- source/server/constants.py
+++ source/server/constants.py
@@ -1,10 +1,9 @@
-LOOP_BACK = '127.0.0.1'
 HOST = '0.0.0.0'
 PORT = 15215
 
 PACKET_LIMIT_SIZE = 512 # bytes
 BYTES_PER_BLOCK = 4096
 
-CONTACT_FILE_PATH = '../data/contact_data.json'
-LARGE_IMAGE_DIR = '../data/large_image'
-SMALL_IMAGE_DIR = '../data/small_image'
+CONTACT_FILE_PATH = 'data/contact_data.json'
+LARGE_IMAGE_DIR = 'data/large_image'
+SMALL_IMAGE_DIR = 'data/small_image'
