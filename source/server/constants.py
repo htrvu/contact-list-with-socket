ab050508c043472214aa7bba9486533208a06ed8
dotrann1412 <dotrann.1412@gmail.com>
2022-05-20 01:06:06 +0700

    add debug flag

--- source/server/constants.py
+++ source/server/constants.py
@@ -1,3 +1,5 @@
+import sys
+
 HOST = '0.0.0.0'
 PORT = 15215
 
@@ -7,3 +9,5 @@ BYTES_PER_BLOCK = 4096
 CONTACT_FILE_PATH = 'data/contact_data.json'
 LARGE_IMAGE_DIR = 'data/large_image'
 SMALL_IMAGE_DIR = 'data/small_image'
+
+DEBUGGING = '-x' in sys.argv
